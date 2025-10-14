39d0c17a9feef06f5ce61689f6c70e6fb27f36e0
CaioSwe <caio.sweiver.carvalho@uel.br>
2025-10-14 11:34:40 -0300

    SVG, Info e ph grafo

--- src/lista.c
+++ src/lista.c
@@ -1,4 +1,4 @@
-#include "Lista.h"
+#include "lista.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,92 +8,102 @@ typedef struct cel{
     struct cel* ant;
 } Celula;
 
-struct Lista{
+typedef struct Lista{
     Celula* inicio;
     Celula* fim;
     int tamanho;
-};
+} listaStr;
 
-Lista* criaLista(){
-    Lista* l = (Lista*)malloc(sizeof(Lista));
+Lista criaLista(){
+    listaStr* l = (listaStr*)malloc(sizeof(listaStr));
     l->inicio = NULL;
     l->fim = NULL;
     l->tamanho = 0;
     return l;
 }
 
-void inserirInicio(Lista* Lista, void* v){
-    Celula *nova = malloc(sizeof(Celula));
+void inserirInicio(Lista l, void* v){
+    Celula* nova = (Celula*)malloc(sizeof(Celula));
     nova->item = v;
 
-    if (Lista->inicio == NULL){ // Lista vazia
-        Lista->inicio = nova;
-        Lista->fim = nova;
+    listaStr* lista = (listaStr*)l;
+
+    if (lista->inicio == NULL){ // lista vazia
+        lista->inicio = nova;
+        lista->fim = nova;
         nova->prox = NULL;
         nova->ant = NULL;
     }
     else{
-        nova->prox = Lista->inicio;
+        nova->prox = lista->inicio;
         nova->ant = NULL;
-        Lista->inicio->ant = nova;
-        Lista->inicio = nova;
+        lista->inicio->ant = nova;
+        lista->inicio = nova;
     }
 
-    Lista->tamanho += 1;
+    lista->tamanho += 1;
 }
 
-void inserirFim(Lista* Lista, void* v){
+void inserirFim(Lista l, void* v){
     Celula* nova = malloc(sizeof(Celula));
     nova->item = v;
 
-    if (Lista->inicio == NULL){
-        Lista->inicio = nova;
-        Lista->fim = nova;
+    listaStr* lista = (listaStr*)l;
+
+    if (lista->inicio == NULL){
+        lista->inicio = nova;
+        lista->fim = nova;
         nova->prox = NULL;
         nova->ant = NULL;
     }
     else{
         nova->prox = NULL;
-        Lista->fim->prox = nova;
-        nova->ant = Lista->fim;
-        Lista->fim = nova;
+        lista->fim->prox = nova;
+        nova->ant = lista->fim;
+        lista->fim = nova;
     }
 
-    Lista->tamanho += 1;
+    lista->tamanho += 1;
 }
 
-void* removerInicio(Lista* Lista){
-    Celula* cel = Lista->inicio;
+void* removerInicio(Lista l){
+    listaStr* lista = (listaStr*)l;
+
+    Celula* cel = lista->inicio;
     void* v = cel->item;
 
-    Lista->inicio = Lista->inicio->prox;
+    lista->inicio = lista->inicio->prox;
 
-    if(Lista->inicio == NULL) // Lista ficará vazia
-        Lista->fim = NULL;
+    if(lista->inicio == NULL) // lista ficará vazia
+        lista->fim = NULL;
     else
-        Lista->inicio->ant = NULL;
+        lista->inicio->ant = NULL;
     
     free(cel);
 
-    Lista->tamanho -= 1;
+    lista->tamanho -= 1;
 
     return v;
 }
 
-void* removerFim(Lista* Lista){
-    Celula* cel = Lista->fim;
+void* removerFim(Lista l){
+    listaStr* lista = (listaStr*)l;
+
+    Celula* cel = lista->fim;
     void* v = cel->item;
 
-    Lista->fim = Lista->fim->ant;
+    lista->fim = lista->fim->ant;
 
     free(cel);
 
-    Lista->tamanho -= 1;
+    lista->tamanho -= 1;
 
     return v;
 }
 
-void* remover(Lista* lista, bool(*f)(const void*, const void*), const void* item){
+void* remover(Lista l, bool(*f)(const void*, const void*), const void* item){
+    listaStr* lista = (listaStr*)l;
+    
     if (!lista) return NULL;
 
     Celula* cel = lista->inicio;
@@ -118,18 +128,22 @@ void* remover(Lista* lista, bool(*f)(const void*, const void*), const void* item
     return NULL;
 }
 
-bool isListaVazia(Lista* lista){
+bool isListaVazia(Lista l){
+    listaStr* lista = (listaStr*)l;
+
     if(lista->inicio == NULL){
         return true;
     }
     return false;
 }
 
-int listaTamanho(Lista* lista){
-    return lista->tamanho;
+int listaTamanho(Lista l){
+    return ((listaStr*)l)->tamanho;
 }
 
-void percorrerLista(Lista* lista, void (*f)(const void*)) {
+void percorrerLista(Lista l, void (*f)(const void*)) {
+    listaStr* lista = (listaStr*)l;
+
     Celula* cel = lista->inicio;
     
     while(cel != NULL){
@@ -138,7 +152,9 @@ void percorrerLista(Lista* lista, void (*f)(const void*)) {
     }
 }
 
-void percorrerListaRel(Lista* lista, void (*f)(const void*, const void*), void* aux) {
+void percorrerListaRel(Lista l, void (*f)(const void*, const void*), void* aux) {
+    listaStr* lista = (listaStr*)l;
+
     Celula* cel = lista->inicio;
     
     while(cel != NULL){
@@ -147,7 +163,9 @@ void percorrerListaRel(Lista* lista, void (*f)(const void*, const void*), void* 
     }
 }
 
-bool isInLista(Lista* lista, bool(*f)(const void*, const void*), const void* valor) {
+bool isInLista(Lista l, bool(*f)(const void*, const void*), const void* valor) {
+    listaStr* lista = (listaStr*)l;
+
     if (!lista) return false;
 
     Celula* cel = lista->inicio;
@@ -161,7 +179,9 @@ bool isInLista(Lista* lista, bool(*f)(const void*, const void*), const void* val
     return false;
 }
 
-void* getItemLista(Lista* lista, int pos){
+void* getItemLista(Lista l, int pos){
+    listaStr* lista = (listaStr*)l;
+
     if (!lista || lista->tamanho < pos) return NULL;
 
     Celula* cel = lista->inicio;
@@ -173,7 +193,9 @@ void* getItemLista(Lista* lista, int pos){
     return cel->item;
 }
 
-void* getItemListaI(Lista* lista, void* element, bool(*f)(const void*, const void*)){
+void* getItemListaI(Lista l, void* element, bool(*f)(const void*, const void*)){
+    listaStr* lista = (listaStr*)l;
+
     if (!lista) return false;
 
     Celula* cel = lista->inicio;
@@ -187,7 +209,22 @@ void* getItemListaI(Lista* lista, void* element, bool(*f)(const void*, const voi
     return NULL;
 }
 
-void limparLista(Lista* lista, bool freeItems) {
+void imprimirLista(Lista l, void(*imprimir)(const void*)){
+    listaStr* lista = (listaStr*)l;
+
+    if (!lista) return;
+
+    Celula* cel = lista->inicio;
+
+    while(cel != NULL){
+        imprimir(cel->item);
+        cel = cel->prox;
+    }
+}
+
+void limparLista(Lista l, bool freeItems) {
+    listaStr* lista = (listaStr*)l;
+
     if (!lista) return;
 
     Celula* atual = lista->inicio;
@@ -203,7 +240,10 @@ void limparLista(Lista* lista, bool freeItems) {
     lista->tamanho = 0;
 }
 
-void copyLista(Lista* from, Lista* to){
+void copyLista(Lista f, Lista t){
+    listaStr* from = (listaStr*)f;
+    listaStr* to = (listaStr*)t;
+
     limparLista(to, false);
 
     to->inicio = from->inicio;
--- src/main.c
+++ src/main.c
@@ -3,31 +3,60 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "MeuGrafo.h"
-#include "MinhaLista.h"
+#include <raylib.h>
+#include "lista.h"
 
-typedef enum Caminhos {
+typedef enum GEOs {
     ENTRADA,
     SAIDA,
-    CAMINHO,
-    QUERY
-} caminhos;
+    GEO,
+    QUERY,
+    VIA
+} GEOs;
 
 typedef struct Info{
+    char* id;
+
+    Rectangle rec;
     char* sw;
     char* cfill;
     char* cstrk;
 }Info;
 
-typedef struct N{
-    Rectangle rec;
-    Info info;
-}n;
+/////////////// GRAFO DE PLACEHOLDER
+
+typedef struct Vertices{
+    int id;
+    double x, y;
+} Vertices;
+
+typedef struct Arestas{
+    char* nome;
+    
+    char* ldir, lesq;
+
+    double cmp;
+    double vm;
+}Arestas;
+
+typedef struct Grafo{
+    int nVertices;
+    int nArestas;
+
+    int maxVertices;
+    
+    Vertices* vertices;
+    Arestas* arestas;
+
+    Lista* adjacencia;
+}Grafo;
+
+///////////////
 
 void printToSVG(const void* item, const void* aux){
-    Rectangle* rec = (Rectangle*)item;
+    Info* info = (Info*)item;
     FILE* arquivo = (FILE*)aux;
-    fprintf(arquivo, "<rect width=\"%.1f\" height=\"%.1f\" x=\"%.1f\" y=\"%.1f\" fill=\"blue\" />\n", rec->width, rec->height, rec->x, rec->y);
+    fprintf(arquivo, "<rect width=\"%.1f\" height=\"%.1f\" x=\"%.1f\" y=\"%.1f\" fill=\"%s\" stroke=\"%s\" stroke-width=\"%s\" />\n", info->rec.width, info->rec.height, info->rec.x, info->rec.y, info->cfill, info->cstrk, info->sw);
 }
 
 float maxX = 0;
@@ -41,21 +70,22 @@ void getMax(const void* item){
 }
 
 int main(int argc, char* argv[]){
-    char* flags[4] = {"-e", "-o", "-f", "-q"};
-    char** paths = (char**)malloc(sizeof(char*) * 4);
+    char* flags[5] = {"-e", "-o", "-f", "-q", "-v"};
+    char** paths = (char**)malloc(sizeof(char*) * 5);
 
-    for(int i = 0; i < 4; i++){
+    for(int i = 0; i < 5; i++){
         paths[i] = (char*)malloc(sizeof(char));
         paths[i] = NULL;
     }
 
-    // Pega todos os nomes e caminhos dos arquivos base e organiza na variável paths
-    // paths[0] = entrada
-    // paths[1] = saida
-    // paths[2] = caminho (absoluto ou relativo)
-    // paths[3] = query
+    // Pega todos os nomes e GEOs dos arquivos base e organiza na variável paths
+    // paths[0] = entrada (absoluto ou relativo) (opcional)
+    // paths[1] = saida (absoluto ou relativo)
+    // paths[2] = GEO
+    // paths[3] = query (opcional)
+    // paths[4] = VIA (opcional)
     for(int i = 1; i < argc; i += 2){
-        for(int j = 0; j < 4; j++){
+        for(int j = 0; j < 5; j++){
             if(strcmp(argv[i], flags[j]) == 0){
                 free(paths[j]);
                 paths[j] = (char*)malloc(sizeof(char) * strlen(argv[i + 1]));
@@ -71,12 +101,19 @@ int main(int argc, char* argv[]){
         strcpy(paths[ENTRADA], "./");
     }
 
+    // Caso o path de saída seja nulo (não fornecido), é escolhido o diretório corrente (./)
+    if(!paths[SAIDA]){
+        free(paths[SAIDA]);
+        paths[SAIDA] = (char*)malloc(sizeof(char) * strlen("./"));
+        strcpy(paths[SAIDA], "./");
+    }
+    
     printf("\nEntrada: %s", paths[ENTRADA]);
     printf("\nSaida: %s", paths[SAIDA]);
-    printf("\nArquivo Geo: %s", paths[CAMINHO]);
+    printf("\nArquivo Geo: %s", paths[GEO]);
     printf("\nQuery: %s", paths[QUERY]);
 
-    // Coloca '/' caso não tenha no final da entrada -e
+    // Coloca '/' caso não tenha no final do path de entrada
     if(paths[ENTRADA][strlen(paths[ENTRADA]) - 1] != '/'){
         strncat(paths[ENTRADA], "/", (size_t)(paths[ENTRADA] + 1));
     }
@@ -84,15 +121,14 @@ int main(int argc, char* argv[]){
     printf("\n\n");
 
     // Coloca ".geo" no arquivo geo caso não tenha
-    strcpy(paths[CAMINHO], changeExtension(paths[CAMINHO], ".geo"));
+    strcpy(paths[GEO], changeExtension(paths[GEO], ".geo"));
     
-    
+
     // (2) Utilizando o .geo
     ////////////////////////////////////////
 
-
-    // Concatena o caminho e a entrada para fEntradaPath
-    const char* fEntradaPath = strcatcat(paths[ENTRADA], paths[CAMINHO]);
+    // Concatena o path de entrada e o nome do arquivo GEO para fEntradaPath
+    const char* fEntradaPath = strcatcat(paths[ENTRADA], paths[GEO]);
 
     // Abre o arquivo de entrada no modo de leitura
     FILE* fEntrada = fopen(fEntradaPath, "r");
@@ -104,28 +140,34 @@ int main(int argc, char* argv[]){
     Rectangle posQuadra;
 
     // Variáveis de espessura da borda, cor de preenchimento e borda, respectivamente.
-    char* sw[256];
-    char* cfill[256];
-    char* cstrk[256];
+    char sw[256];
+    char cfill[256];
+    char cstrk[256];
 
     // Lista genérica das formas lidas no arquivo .geo
     Lista* formas = criaLista();
 
     while(fscanf(fEntrada, "%s", op) > 0){
         if(strcmp(op, "cq") == 0){
-            scanf(fEntrada, "%s %s %s\n", sw, &cfill, &cstrk);
+            fscanf(fEntrada, "%s %s %s\n", sw, cfill, cstrk);
         }
         else if(strcmp(op, "q") == 0){
             fscanf(fEntrada, "%s %f %f %f %f\n", quadraID, &posQuadra.x, &posQuadra.y, &posQuadra.width, &posQuadra.height);
             
-            Rectangle* quadra = (Rectangle*)malloc(sizeof(Rectangle));
+            Info* info = (Info*)malloc(sizeof(Info));
             
-            quadra->x = posQuadra.x;
-            quadra->y = posQuadra.y;
-            quadra->width = posQuadra.width;
-            quadra->height = posQuadra.height;
+            //info->id = quadraID;
 
-            inserirFim(formas, quadra);
+            info->rec.x = posQuadra.x;
+            info->rec.y = posQuadra.y;
+            info->rec.width = posQuadra.width;
+            info->rec.height = posQuadra.height;
+
+            info->cfill = cfill;
+            info->sw = sw;
+            info->cstrk = cstrk;
+
+            inserirFim(formas, info);
         }
     }
 
@@ -137,7 +179,7 @@ int main(int argc, char* argv[]){
     /////////////////////////////////////////
 
     // Concatena o diretório de saída com o nome do arquivo .geo em formato .svg
-    const char* fOutputPath = strcatcat(paths[SAIDA], changeExtension(paths[CAMINHO], ".svg"));
+    const char* fOutputPath = strcatcat(paths[SAIDA], changeExtension(paths[GEO], ".svg"));
     
     // Abre o diretório em modo de escrita
     FILE* fSaida = fopen(fOutputPath, "w");
